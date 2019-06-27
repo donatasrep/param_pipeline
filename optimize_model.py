b715483871a1178f93154b992b551f554d8247a8
ASB <xirmantasx@gmail.com>
2019-06-27 11:22:21 +0300

    Improved time loginh

--- optimize_model.py
+++ optimize_model.py
@@ -21,7 +21,17 @@ import logging
 from hyperopt import hp, tpe, fmin, Trials
 from hyperopt import STATUS_OK, STATUS_FAIL
 
-logging.basicConfig(stream=sys.stderr, format='%(asctime)s-%(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
+try:
+    import absl.logging
+
+    # https://github.com/abseil/abseil-py/issues/99
+    logging.root.removeHandler(absl.logging._absl_handler)
+    # https://github.com/abseil/abseil-py/issues/102
+    absl.logging._warn_preinit_stderr = False
+except Exception:
+    pass
+
+logging.basicConfig(stream=sys.stdout, format='%(asctime)s-%(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
 
 #tensorflow configuration
 config = tf.ConfigProto()
