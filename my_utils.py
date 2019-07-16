0075cf4d06b7b746624a5a2209fe9896a86e787c
ASB <xirmantasx@gmail.com>
2019-07-16 09:38:12 +0300

    Reverse

--- my_utils.py
+++ my_utils.py
@@ -275,11 +275,12 @@ class DataGenerator(Sequence):
 
     def reverse(self, x):
 
-        print(x)
+
         if self.reverse_p > np.random.rand():
+            print(x)
             for i in range(len(x)):
                 x[i] = N_MAP[x[i]]
-        print(x)
+            print(x)
         return x
 
     def random_mutations(self, x):
